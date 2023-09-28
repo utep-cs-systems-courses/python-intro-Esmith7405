579fd81e2336196164104b5c7ba0a6efed281c0d
esmith7405 <ethansmith122601@gmail.com>
2023-09-28 13:36:49 -0600

    Started OS calls, fixed punctuation-parse issues

--- wordCountOS.py
+++ wordCountOS.py
@@ -18,18 +18,26 @@ outputFname = sys.argv[2]
 wordCounts = {}
 
 # attempt to open input file
-with open(textFname, 'r') as inputFile:
+#with open(textFname, 'r') as inputFile:
+inputFile = os.open(textFname, os.O_RDONLY)
+n = 50000
+readIn = os.read(inputFile, n) #Assign the file as a string
+docIn = readIn.decode("utf-8","strict")
+
+"""
     for line in inputFile:
-        line = line.lower() #convert word to lowercase and remove punctuation
         line = line.strip() # get rid of newline characters
-        splitLine = re.split("[ \t" + string.punctuation + " ]", line) # split line on whitespace and punctuation - DOES NOT SEPARATE DASHED DOUBLE WORDS
+        splitLine = re.split("[ \t]", line) # split line on whitespace and punctuation - DOES NOT SEPARATE DASHED DOUBLE WORDS
         #run through the line and update Word Counts
         for word in splitLine:
+            word = word.lower().translate(str.maketrans('','', string.punctuation)) #convert word to lowercase and remove punctuation
+            #word = word.lower()
             #if the word is in the dictionary, increment it's count, otherwise, add it to the dictionary
             if word in wordCounts:
                 wordCounts[word]+=1
             else:
                 wordCounts[word] = 1
+
 inputFile.close()
 wordCounts.pop('')
 #print(wordCounts.items())
@@ -37,3 +45,4 @@ with open(outputFname, 'w') as outputFile:
     for key in sorted(wordCounts.keys()):
         outputFile.write(key + " " + str(wordCounts[key]) + "\n")
 outputFile.close()
+"""
